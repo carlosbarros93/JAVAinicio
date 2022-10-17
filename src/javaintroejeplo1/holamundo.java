97ab6f8b2e67e9835b068aecdeda306b012d595d
carlos barros <carlos.barros625@gmail.com>
2022-10-17 19:14:49 -0300

    commit1

---
+++ src/javaintroejeplo1/holamundo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*java introejemplo1 seria el paquete donde se van a contener las clases*/
+package javaintroejeplo1;
+
+/**
+ *
+ * @author USUARIO
+ */
+/*hola mundo seria las clases donde se van a contener los objetos*/
+public class holamundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    /*el main vendria a ser el cominezo del programa,
+    sin el mail las sentencias que nosotros escribamos abajo no se van a ejutar.*/
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
